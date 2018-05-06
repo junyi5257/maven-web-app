62f5884cde082ba4b3dd3abc9d2ddf4fd99ddef1
junyi5257 <junyi5257@163.com>
2018-05-06 18:52:56 +0800

    Commit    init  code

--- maven-thread/src/main/java/cn/msb/chap01/HelloWorld.java
+++ maven-thread/src/main/java/cn/msb/chap01/HelloWorld.java
@@ -4,19 +4,25 @@ package cn.msb.chap01;
  * HelloWorld
  * Created by goujy on 8/5/17.
  */
+
 import java.util.Random;
 
+/**
+ * @author goujy
+ */
 public class HelloWorld {
-    public static void main(String ... args) {
-        System.out.println(randomString(-229985452)+' '+randomString(-147909649));
+    public static void main(String... args) {
+        System.out.println(randomString(-229985452) + ' ' + randomString(-147909649));
     }
 
     public static String randomString(int seed) {
         Random rand = new Random(seed);
         StringBuilder sb = new StringBuilder();
-        while(true) {
+        while (true) {
             int n = rand.nextInt(27);
-            if (n == 0) break;
+            if (n == 0) {
+                break;
+            }
             sb.append((char) ('`' + n));
         }
         return sb.toString();
--- src/main/java/cn/junyi/mavenWeb/reflect/BaseBeanB.java
+++
@@ -1,7 +0,0 @@
-package cn.junyi.mavenWeb.reflect;
-
-/**
- * Created by goujy on 7/3/17.
- */
-public class BaseBeanB {
-}
